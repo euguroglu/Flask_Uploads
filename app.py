38604ea34d7fd864345fdee315f6c3433d35a315
euguroglu <e.uguroglu@yahoo.com>
2020-12-26 18:54:29 +0300

    allow and deny added

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template,request
-from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_uploads import UploadSet, configure_uploads, IMAGES, UploadNotAllowed
 
 app = Flask(__name__)
 
 photos = UploadSet('photos',IMAGES)
 
 app.config['UPLOADED_PHOTOS_DEST'] = 'pictures'
+app.config['UPLOADED_PHOTOS_ALLOW'] = ['txt']
+app.config['UPLOADED_PHOTOS_DENY'] = ['png']
 
 configure_uploads(app,photos)
 
 @app.route('/upload',methods=['GET','POST'])
 def upload():
     if request.method == 'POST' and 'thefile' in request.files:
-        image_filename = photos.save(request.files['thefile'])
-        return '<h1>'+image_filename+'</h1>'.format(image_filename)
+        try:
+            image_filename = photos.save(request.files['thefile'])
+            return '<h1>'+image_filename+'</h1>'.format(image_filename)
+        except UploadNotAllowed:
+            return '<h1>File is not allowed</h1>'
     return render_template('upload.html')
 
 if __name__ == "__main__":
