0d154ca00bf18e71a7d4390d3b1353cde810649b
euguroglu <e.uguroglu@yahoo.com>
2020-12-26 17:54:21 +0300

    basic upload implemented

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
@@ -8,6 +8,10 @@ photos = UploadSet('photos',IMAGES)
 app.config['UPLOADED_PHOTOS_DEST'] = '/pictures'
 
 configure_uploads(app,photos)
+
+@app.route('/upload',methods=['GET','POST'])
+def upload():
+    return render_template('upload.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
